146ef3907d96415c51ffb4f9892f02bf139772b5
mitravajjala <69940532+mitravajjala@users.noreply.github.com>
2022-04-30 19:07:11 -0500

    Update install_rbiotools.sh

--- install_rbiotools.sh
+++ install_rbiotools.sh
@@ -1,13 +1,7 @@
 source activate metabp_env
-
 module load mamba
-
 mamba install -c r r
-
 mamba install -c r r-rcpp r-rcurl r-gplots r-gridextra
-
 mamba install -c bioconda bioconductor-msa bioconductor-biostrings r-fmsb r-rentrez r-pheatmap r-seqinr 
-
 mamba install -c conda-forge r-stringr r-grimport r-data.table r-ape r-ggplot2 
- 
 conda deactivate
