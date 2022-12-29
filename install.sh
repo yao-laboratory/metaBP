31d2ee936b7aa5dcd414315d5d5188a05f4a4443
cweyers2 <120822794+cweyers2@users.noreply.github.com>
2022-12-28 22:00:13 -0600

    Add permissions in install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 module load mamba
 conda create -n metabp_env
 source activate metabp_env
+chmod +x bbmap/bbmap/*.sh
+chmod +x plass/bin/plass
+chmod +x mmseqs/bin/mmseqs
+chmod +x clustalo/clustalo-1.2.4-Ubuntu-x86_64
 mamba install python=3.9 
 mamba install pandas
 mamba install -c bioconda eggnog-mapper
